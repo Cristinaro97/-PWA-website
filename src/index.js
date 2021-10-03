79ed70b8681683b65918a57073e743008c472091
Cristinaro97 <cristinagrl@hotmail.com>
2021-10-02 21:53:06 -0500

    pantallas y css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-function sum(a, b) {
-return a + b;
+const style = require("./style.css");
+
+/*Access to screen*/
+const viewSignUp = document.querySelector(".signUp");
+const viewSignIn = document.querySelector(".signIn");
+const vieWebNotes = document.querySelector(".webNotes");
+
+/* Btns input valur*/
+const getName = document.getElementById("fname");
+const getPassword = document.getElementById("lpassword");
+
+/*Btns form*/
+const getUserData = document.getElementById("getUserData");
+const submitUserData = document.getElementById("submitUserData");
+
+const btnSignUp = document.querySelectorAll(".btnViewLogin");
+const btnSignIn = document.querySelectorAll(".btnViewnSignUp");
+
+viewSignIn.style.display = "none";
+vieWebNotes.style.display = "none";
+
+for (let i = 0; i < btnSignUp.length; i++) {
+  let element = btnSignUp[i];
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+    viewSignIn.style.display = "block";
+    viewSignUp.style.display = "none";
+    vieWebNotes.style.display = "none";
+  });
 }
 
-console.log(sum(8, 2));
+for (let i = 0; i < btnSignIn.length; i++) {
+  let element = btnSignIn[i];
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+    viewSignIn.style.display = "none";
+    viewSignUp.style.display = "none";
+    vieWebNotes.style.display = "block";
+  });
+}
+
+getUserData.addEventListener("click", function (e) {
+  e.preventDefault();
+  localStorage.setItem("name", getName.value);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin"); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
 
 module.exports = {
     entry: './src/index.js',
@@ -9,5 +11,38 @@ module.exports = {
     resolve: {
         extensions: ['.js']
     },
-    
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/i,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.html/i,
+            use: [
+              {
+                loader: "html-loader",
+                options: {
+                  minimize: true,
+                },
+              },
+            ],
+          },
+          { 
+              test: /\.css$/i, 
+              use: [MiniCssExtractPlugin.loader, "css-loader"
+            ], 
+            }, 
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin(), 
+      ],
 }
